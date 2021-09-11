97e21e7d58ea46a57437b70c51bca8e1a5381b08
Jorge Pauvels <jcvels@uvcoding.com.ar>
2021-09-11 19:06:47 -0300

    update main script

--- start_test.cmd
+++ start_test.cmd
@@ -14,11 +14,23 @@ C:\Windows\System32\powercfg.exe /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 :: SCRIPT CONFIG
 set TEST=PRUEBA PRUEBA
 set DIR=%CD%
+set WLAN=Wifi2.4
+set PING=1.1.1.1
 
 :: START
 echo.
 echo. [%TIME%] Starting ASUS Notebook Testing Script for MP
 echo. [%TIME%] El directorio de trabajo actual es %DIR%
+
+:: WIFI CONECTION
+echo. [%TIME%] Iniciando conección de red.
+netsh wlan connect profile=%WLAN% name=%WLAN%
+ping %PING% > null
+if not %errorlevel% == 0 (
+
+    goto FAIL
+)
+
 
 :: FAN - Validacion de funcionamiento, velocidad encendido y apagado del system fan
 echo. [%TIME%] Ejecutando validacion del system fan. (fan_test.cmd)
@@ -31,3 +43,7 @@ cmd.exe /c %DIR%\BIOS\bios_test.cmd
 :: DEBUG
 echo. [%TIME%] Ejecutando nombre_del_test. (nombre_del_test.cmd)
 timeout 10 > null
+
+:: FAIL
+:FAIL
+%CD%\MISC\fail_msg.cmd
