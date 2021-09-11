f60c317db693f4adc7e06bce9ececf56d4a1aad5
Jorge Pauvels <jcvels@uvcoding.com.ar>
2021-09-11 17:46:00 -0300

    update bios_test

--- BIOS/bios_test.cmd
+++ BIOS/bios_test.cmd
@@ -6,7 +6,8 @@
 @echo off
 
 :: CONFIGURACION
-set TOOL=%CD%\BIOS\TOOLS\WBT.exe
+set TOOL_1=%CD%\BIOS\TOOLS\WBT.exe
+set TOOL_2=%CD%\BIOS\TOOLS\WBT.exe
 set VERSION=%CD%\BIOS\TOOLS\bios_version
 set MODEL=%CD%\BIOS\TOOLS\bios_model
 set TEMP=%CD%\BIOS\TOOLS\on_test.tmp
@@ -15,7 +16,7 @@ set LOG=%CD%\BIOS\bios_test.log
 :: TESTING PROCCESS
 
     :: dump bios mfg block
-    %TOOL% /dump > %TEMP%
+    %TOOL_1% /dump > %TEMP%
     timeout 1 >> %LOG%
 
     :: check if bios version is the spected
@@ -34,9 +35,15 @@ set LOG=%CD%\BIOS\bios_test.log
         goto FAIL
     )
 
-    :: TODO: Configuración de fecha y hora
+    :: TODO: Configuración de fecha y hora ################################ <<<---##
 
-    
+    :: set bios to default and validate
+    %TOOL_1% /def > %TEMP%
+    timeout 1 >> %LOG%
+    find /C "Return Code = 0" %TEMP% >> %LOG%
+    if not %errorlevel% == 0 (
+        goto FAIL
+    )
 
 :: PASS
 :PASS
