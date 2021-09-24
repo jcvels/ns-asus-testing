b6fd7628acb8f7dd4481d217dff43feea5e4bbe2
Jorge Pauvels <jcvels@uvcoding.com.ar>
2021-09-23 22:18:45 -0300

    update updater and config

--- update.cmd
+++ update.cmd
@@ -8,5 +8,8 @@ del %CD%\OA3\TOOLS\on_test.xml
 git checkout *.*
 git pull
 
+%CD%\BIOS\TOOLS\wbt.exe /dump > %CD%\BIOS\TOOLS\bios_version
+wmic baseboard get product,manufacturer,version > %CD%\BIOS\TOOLS\bios_model
+
 timeout 10
 shutdown -r -f -t 0
