cd98137266df2b7235632dd4b3fe05257d359017
Jorge Claudio Pauvels <jcvels@uvcoding.com.ar>
2021-10-16 22:06:37 -0300

    add readme

--- IMAGE/change_boot.cmd
+++ IMAGE/change_boot.cmd
@@ -1,2 +1,27 @@
 @echo off
-timeout 20
+echo (%TIME%) Iniciando ...
+
+:: Montar particion de arranque del disco
+echo (%TIME%) Montando disco de arranque en S: ...
+mountvol S: /s
+
+:: Eliminar archivos de arranque actuales
+echo (%TIME%) Eliminanndo archivos de arranque actuales ...
+DEL /F /S /Q S:\EFI\*.*
+
+:: Crear carpeta y copiar archivos de la aplicacion shell.efi
+echo (%TIME%) Copiando archivos de arranque nuevos ...
+IF NOT EXIST S:\EFI\Boot mkdir S:\EFI\Boot
+copy /Y uefi_shell_min.efi S:\EFI\Boot\bootx64.efi
+copy /Y profiles.txt S:\EFI\Boot\profiles.txt
+
+:: Validación
+IF ERRORLEVEL 1 GOTO ERR 
+
+echo (%TIME%) El proceso finalizó sin errores.
+exit
+
+:: Si ocurrio un error
+:ERR
+echo (%TIME%) Ocurio un error durante el proceso.
+pause
