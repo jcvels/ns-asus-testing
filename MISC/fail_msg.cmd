f4f9fd265e26ae329dd99199527028fc072047ec
Jorge Claudio Pauvels <jcvels@uvcoding.com.ar>
2021-10-21 01:54:09 -0300

    update fail message

--- MISC/fail_msg.cmd
+++ MISC/fail_msg.cmd
@@ -5,5 +5,8 @@ echo. -- FAIL -- FAIL -- FAIL -- FAIL -- -- FAIL -- FAIL -- FAIL -- FAIL ---- FA
 echo. FAIL -- FAIL -- FAIL -- FAIL -- -- FAIL -- FAIL -- FAIL -- FAIL ---- FAIL -- FAIL -- FAIL -- FAIL -- FAIL -- FAIL --
 echo.
 start wmplayer "C:\Windows\Media\Alarm10.wav" && timeout 5 > null && taskkill /im wmplayer.exe > null
+echo. Presione una tecla para apagar el equipo...
+echo.
 pause
+shutdown -s -f -t 0
 exit
